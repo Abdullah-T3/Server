7fa3ff03bb57dfbd2f7493cacd1a551f6ad8c243
Abdullah-T3 <162081172+Abdullah-T3@users.noreply.github.com>
2024-09-13 19:10:03 +0300

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,6 @@
+// controllers/userController.js
+
 const db = require('../config/db');
-const bcrypt = require('bcrypt');
 
 // Controller to validate user login
 exports.validateUser = (req, res) => {
@@ -9,7 +10,7 @@ exports.validateUser = (req, res) => {
         return res.status(400).json({ error: 'Username and password are required' });
     }
 
-    const query = 'SELECT password FROM users WHERE username = ?';
+    const query = 'SELECT password FROM users WHERE user_name = ?';
 
     db.query(query, [username], (err, results) => {
         if (err) {
@@ -20,19 +21,11 @@ exports.validateUser = (req, res) => {
         if (results.length > 0) {
             const storedPassword = results[0].password;
 
-            // Use bcrypt to compare the hashed password
-            bcrypt.compare(password, storedPassword, (err, isMatch) => {
-                if (err) {
-                    console.error('Error comparing passwords:', err);
-                    return res.status(500).json({ error: 'Error comparing passwords' });
-                }
-
-                if (isMatch) {
-                    return res.json({ success: true, message: 'Login successful' });
-                } else {
-                    return res.status(401).json({ success: false, message: 'Invalid password' });
-                }
-            });
+            if (password == storedPassword) {
+                return res.json({ success: true, message: 'Login successful' });
+            } else {
+                return res.status(401).json({ success: false, message: 'Invalid password' });
+            }
         } else {
             return res.status(404).json({ success: false, message: 'User not found' });
         }
