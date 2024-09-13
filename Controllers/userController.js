3fc1f253fbd88e1edecdf19cfb6ed0ff285de3c3
Abdullah-T3 <eng.abdullahahmed59@gmail.com>
2024-09-13 10:18:25 +0300

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,5 @@
-// controllers/userController.js
-
 const db = require('../config/db');
+const bcrypt = require('bcrypt');
 
 // Controller to validate user login
 exports.validateUser = (req, res) => {
@@ -21,11 +20,19 @@ exports.validateUser = (req, res) => {
         if (results.length > 0) {
             const storedPassword = results[0].password;
 
-            if (password == storedPassword) {
-                return res.json({ success: true, message: 'Login successful' });
-            } else {
-                return res.status(401).json({ success: false, storedPassword, message: 'Invalid password' });
-            }
+            // Use bcrypt to compare the hashed password
+            bcrypt.compare(password, storedPassword, (err, isMatch) => {
+                if (err) {
+                    console.error('Error comparing passwords:', err);
+                    return res.status(500).json({ error: 'Error comparing passwords' });
+                }
+
+                if (isMatch) {
+                    return res.json({ success: true, message: 'Login successful' });
+                } else {
+                    return res.status(401).json({ success: false, message: 'Invalid password' });
+                }
+            });
         } else {
             return res.status(404).json({ success: false, message: 'User not found' });
         }
