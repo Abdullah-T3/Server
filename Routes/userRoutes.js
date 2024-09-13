89fe035a1ac6438ea20332f967af1bd73e6c465a
Abdullah-T3 <eng.abdullahahmed59@gmail.com>
2024-09-13 09:26:25 +0300

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -6,7 +6,6 @@ const userController = require('../Controllers/userController');
 // Define the route for validating the user
 router.get('/validate-user', userController.validateUser);
 // Routes/carRoutes.js
-const express = require('express');
 
 const carController = require('../controllers/carController');
 
