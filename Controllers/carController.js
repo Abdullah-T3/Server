160fd96a309fe46821cd84c5be27e26754ad6385
Abdullah-T3 <162081172+Abdullah-T3@users.noreply.github.com>
2024-09-14 08:01:12 +0300

    Update carController.js

--- Controllers/carController.js
+++ Controllers/carController.js
@@ -1,14 +1,4 @@
-const pool = require('../config/db');
 
-function getAllCars(req, res) {
-    pool.query('SELECT * FROM cars', (err, results) => {
-        if (err) {
-            console.error('Error fetching cars:', err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
-        res.json(results);
-    });
-}
 
 const pool = require('../config/db');
 
