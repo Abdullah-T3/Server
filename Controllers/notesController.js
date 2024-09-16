77e5e33532fe8a12df6601ebf7211553b4c810b3
Abdullah-T3 <162081172+Abdullah-T3@users.noreply.github.com>
2024-09-16 15:31:36 +0300

    Update notesController.js

--- Controllers/notesController.js
+++ Controllers/notesController.js
@@ -25,9 +25,9 @@ exports.getNoteById = async (req, res) => {
 
 // Add a new note
 exports.addNote = async (req, res) => {
-    const { title, content } = req.body;
+    const { title, content,deadline } = req.body;
     try {
-        const result = await db.query('INSERT INTO notes (title, content) VALUES (?, ?)', [title, content]);
+        const result = await db.query('INSERT INTO notes (title, content) VALUES (?, ?, ?)', [title, content,deadline]);
         res.status(201).json({ message: 'Note created', id: result.insertId });
     } catch (error) {
         res.status(500).json({ message: 'Error creating note', error });
