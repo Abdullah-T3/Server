4d8e0dae9798b2d1cf00406aa310eef6aeefeb0e
Abdullah-T3 <162081172+Abdullah-T3@users.noreply.github.com>
2024-09-21 12:04:34 +0300

    task

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const usersRouter = require('./Routes/users');
 const customersRouter = require('./Routes/customers');
 const carsRouter = require('./Routes/cars');
 const bookingsRouter = require('./Routes/bookings');
+const tasksRoutes = require('./Routes/tasks');
 require('dotenv').config();
 
 const app = express();
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Routes
+app.use('/api/tasks', tasksRoutes);
 app.use('/api/users', usersRouter);
 app.use('/api/customers', customersRouter);
 app.use('/api/cars', carsRouter);
