6c541005b80184b6e70efa6ae1215568918e0a01
Abdullah-T3 <162081172+Abdullah-T3@users.noreply.github.com>
2024-09-25 19:32:31 +0300

    //

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const carsRouter = require('./Routes/cars');
 const bookingsRouter = require('./Routes/bookings');
 const tasksRoute = require('./Routes/tasks');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use('/api/users', usersRouter);
