3d06df75fa912d736e1734f2499396490eaa1baa
Abdullah-T3 <162081172+Abdullah-T3@users.noreply.github.com>
2024-10-30 00:20:42 +0300

    ..

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,10 +1,11 @@
+require('dotenv').config(); // Load environment variables
+
 const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
 
 cloudinary.config({
-  cloud_name: process.env.cloud_name,
-  api_key: process.env.api_key,       // replace with your API key
-  api_secret: process.env.api_secret,   // replace with your API secret
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 module.exports = cloudinary;
